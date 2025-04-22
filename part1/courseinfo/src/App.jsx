7158dba4509439811a892a462f1874401765701f
Giuliano <yularossi@gmail.com>
2025-04-22 10:17:15 -0300

    Informacion del Curso, paso 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,8 +9,32 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
+      <Part1 part1 = {props.part1} exercises1 = {props.exercises1}/>
+      <Part2 part2 = {props.part2} exercises2 = {props.exercises2}/>
+      <Part3 part3 = {props.part3} exercises3 = {props.exercises3}/>
+    </>
+  )
+}
+
+const Part1 = (props) => {
+  return (
+    <>
       <p>{props.part1} {props.exercises1}</p>
+    </>
+  )
+}
+
+const Part2 = (props) => {
+  return (
+    <>
       <p>{props.part2} {props.exercises2}</p>
+    </>
+  )
+}
+
+const Part3 = (props) => {
+  return (
+    <>
       <p>{props.part3} {props.exercises3}</p>
     </>
   )
