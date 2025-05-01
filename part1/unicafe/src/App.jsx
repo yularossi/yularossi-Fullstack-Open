0e69810840a1a5a538cbb67da5ff0959795f6908
Giuliano <yularossi@gmail.com>
2025-05-01 15:48:28 -0300

    unicafe paso 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+const Header = props => <h1>{props.text}</h1>
+
 const Button = (props) => {
   console.log(props)
   return (
@@ -12,9 +14,9 @@ const Button = (props) => {
 const StatisticsLine = (props) => {
   console.log(props)
   return (
-    <p>
-      {props.text} {props.value}
-    </p>
+    <tr>
+      <td>{props.text}</td><td>{props.value}</td>
+    </tr>
   )
 }
 
@@ -23,19 +25,21 @@ const Statistics = (props) => {
   if (props.all === 0) {
     return (
       <div>
-        <h1>statistics</h1>
         <p>No feedback given</p>
       </div>
     )
   } return (
       <div>
-        <h1>statistics</h1>
-        <StatisticsLine text='good' value={props.good} />
-        <StatisticsLine text='neutral' value={props.neutral} />
-        <StatisticsLine text='bad' value={props.bad} />
-        <StatisticsLine text='all' value={props.all} />
-        <StatisticsLine text='average' value={props.average} />
-        <StatisticsLine text='positive' value={props.positive + ' %'} />
+        <table>
+          <tbody>
+          <StatisticsLine text='good' value={props.good} />
+          <StatisticsLine text='neutral' value={props.neutral} />
+          <StatisticsLine text='bad' value={props.bad} />
+          <StatisticsLine text='all' value={props.all} />
+          <StatisticsLine text='average' value={props.average} />
+          <StatisticsLine text='positive' value={props.positive + ' %'} />
+          </tbody>
+        </table>
       </div>
     )
 }
@@ -45,16 +49,18 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
   const all = good + neutral + bad
   const average = (good - bad) / (good + neutral + bad)
   const positive = (good / (good + neutral + bad)) * 100
 
   return (
     <div>
-      <h1>give feedback</h1>
+      <Header text='give feedback' />
       <Button handleClick={() => setGood(good + 1)} text='good' />
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Header text='statistics' />
       <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
     </div>
   )
