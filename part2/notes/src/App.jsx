162512990b339b63cb6210b0c313e4c3bc663d91
Giuliano <yularossi@gmail.com>
2025-05-13 12:28:20 -0300

    La Agenda Telefonica Paso 4

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -3,10 +3,15 @@ import Note from './components/Note'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState(persons)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -27,6 +32,19 @@ const App = () => {
       setNewNumber('')
     }
   }
+
+  const addFilter = (event) => {
+    event.preventDefault()
+    const filterObject = {
+      filter : newFilter
+    }
+
+    if (newFilter === '') {
+      setFilteredPersons(persons)
+    } else {
+      setFilteredPersons(persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase())))
+    }
+  }
     
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -38,9 +56,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+    
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={addFilter}>
+        <div>
+          filter shown with: <input 
+            value={newFilter}
+            onChange={handleFilterChange}
+          />
+        </div>
+      </form>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -60,7 +93,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => 
+        {filteredPersons.map((person) => 
           <Note key={person.name} person={person} />
         )}
       </ul>
