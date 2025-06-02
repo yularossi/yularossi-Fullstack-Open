f93fdfdb16ad33f7d5024976c97bbcb1730dc108
Giuliano <yularossi@gmail.com>
2025-06-01 22:58:18 -0300

    countries step 3 (not working)

--- part2/countries/src/services/weather.js
+++ part2/countries/src/services/weather.js
@@ -1,15 +1,17 @@
 import axios from 'axios'
-//import dotenv from 'dotenv'
-//dotenv.config()
 
-const baseUrl = 'http://api.weatherapi.com/v1/current.json?key=8f762e3602a54c7cb0c195045250106&q=London&aqi=no'
+// const apiKey = '8f762e3602a54c7cb0c195045250106'
+const getWeather = (country) => {
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY
+// const baseUrl = 'http://api.weatherapi.com/v1/current.json?key=8f762e3602a54c7cb0c195045250106&q=London&aqi=no'
+const baseUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${country}&aqi=no`
+  // const url = `${baseUrl}&q=${country}`
+  const response = axios.get(baseUrl).then(res => res.data).catch(error => {
+    console.error('Error fetching weather data:', error)
+      })
+      console.log('Weather data:', response)
+  return response
+}   
 
-const apiKey = '8f762e3602a54c7cb0c195045250106'
-const getWeather = async (country) => {
-  const url = `${baseUrl}&q=${country}`
-  const response = await axios.get(url)
-  return response.data
-}
-
-const weatherService = { getWeather }
+const weatherService = {getWeather}
 export default weatherService
