ce69089e06caed3df281073bf6ebb4a882957d14
Giuliano <yularossi@gmail.com>
2025-06-18 10:34:33 -0300

    phonebook backend step 8

--- part3/index.js
+++ part3/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
-// Middleware to log requests
-app.use(morgan('tiny'))
+morgan.token('post', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+
+// Custom morgan format to log POST request bodies
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
 app.use(express.json())
 
