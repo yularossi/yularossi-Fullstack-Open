d6458ab397880fa21100e3574714650566f69b5a
Giuliano <yularossi@gmail.com>
2026-05-05 18:53:41 -0300

    Helper Functions and Unit Tests, step 3

--- part4/backend/tests/list_helper.test.js
+++ part4/backend/tests/list_helper.test.js
@@ -45,3 +45,49 @@ describe('total likes', () => {
         assert.strictEqual(result, 15)
     })
 })
+
+describe('favorite blog', () => {
+    test('of empty list is null', () => {
+        const blogs = []
+        const result = listHelper.favoriteBlog(blogs)
+        assert.strictEqual(result, null)
+    })
+    test('when list has only one blog equals that blog', () => {
+        const blogs = [
+            {
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Statement_Con sidered_Harmful.html',
+                likes: 5
+            }
+        ]
+        const result = listHelper.favoriteBlog(blogs)
+        assert.deepStrictEqual(result, {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5
+        })
+    })
+    test('of a bigger list is found right', () => {
+        const blogs = [
+            {
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Statement_Con sidered_Harmful.html',
+                likes: 5
+            },
+            {
+                title: 'Python for Data Analysis',
+                author: 'Wes McKinney',
+                url: 'https://wesmckinney.com/blog/python-for-data-analysis/',
+                likes: 10
+            }
+        ]
+        const result = listHelper.favoriteBlog(blogs)
+        assert.deepStrictEqual(result, {
+            title: 'Python for Data Analysis',
+            author: 'Wes McKinney',
+            likes: 10
+        })
+    })
+})
--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -6,7 +6,23 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    } else {
+        const favorite = blogs.reduce((favorite, blog) => {
+            return blog.likes > favorite.likes ? blog : favorite
+        }, blogs[0])
+        return {
+            title: favorite.title,
+            author: favorite.author,
+            likes: favorite.likes
+        }
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
