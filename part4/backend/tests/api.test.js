2cb7153dc8a143d5c924a93409783365c8336a15
Giuliano <yularossi@gmail.com>
2026-05-06 20:37:21 -0300

    Blog List Tests, step 2

--- part4/backend/tests/api.test.js
+++ part4/backend/tests/api.test.js
@@ -5,11 +5,9 @@ const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
 const config = require('../utils/config')
-
 const api = supertest(app)
 
-describe('Blog API', () => {
-  beforeEach(async () => {
+beforeEach(async () => {
     // Clear database before each test
     await Blog.deleteMany({})
     
@@ -30,8 +28,9 @@ describe('Blog API', () => {
     ]
     
     await Blog.insertMany(testBlogs)
-  })
+})
 
+describe('Blog API', () => {
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -44,8 +43,21 @@ describe('Blog API', () => {
     
     assert.strictEqual(response.body.length, 2)
   })
+})
 
-  after(async () => {
-    await mongoose.connection.close()
+describe ('id field', () => {
+  test('identifier is named id', async () => {
+    const response = await api.get('/api/blogs')
+    
+    //verify that the return is an array
+    assert.strictEqual(Array.isArray(response.body), true, 'Response is not an array')
+    
+    response.body.forEach(blog => {
+      assert.ok(blog.id, 'Blog does not have an id field')
+    })
   })
 })
+
+after(async () => {
+    await mongoose.connection.close()
+  })
