4e4ee33b9ccb85f93cbdf1a3c9240d125f91ea89
Giuliano <yularossi@gmail.com>
2026-05-26 11:45:42 -0300

    Blog List Frontend, step 5 & 6

--- part5/src/components/BlogForm.jsx
+++ part5/src/components/BlogForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 
 const BlogForm = ({ createBlog }) => {
+  const [blogVisible, setBlogVisible] = useState(false)
+  const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogVisible ? '' : 'none' }
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [url, setUrl] = useState('')
@@ -11,47 +14,56 @@ const BlogForm = ({ createBlog }) => {
     setTitle('')
     setAuthor('')
     setUrl('')
+    setBlogVisible(false)
   }
 
   return (
-    <div className="blog-form">
-      <h3>Create a new blog</h3>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Title
-            <input
-              type="text"
-              value={title}
-              name="Title"
-              onChange={({ target }) => setTitle(target.value)}
-            />
-          </label>
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setBlogVisible(true)}>create a new blog</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div className="blog-form">
+          <h3>Create a new blog</h3>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>
+                Title
+                <input
+                  type="text"
+                  value={title}
+                  name="Title"
+                  onChange={({ target }) => setTitle(target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Author
+                <input
+                  type="text"
+                  value={author}
+                  name="Author"
+                  onChange={({ target }) => setAuthor(target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                URL
+                <input
+                  type="text"
+                  value={url}
+                  name="Url"
+                  onChange={({ target }) => setUrl(target.value)}
+                />
+              </label>
+            </div>
+            <button type="submit">create</button>
+          </form>
+          <button onClick={() => setBlogVisible(false)}>cancel</button>
         </div>
-        <div>
-          <label>
-            Author
-            <input
-              type="text"
-              value={author}
-              name="Author"
-              onChange={({ target }) => setAuthor(target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            URL
-            <input
-              type="text"
-              value={url}
-              name="Url"
-              onChange={({ target }) => setUrl(target.value)}
-            />
-          </label>
-        </div>
-        <button type="submit">create</button>
-      </form>
+      </div>
     </div>
   )
 }
