367b423b7b13c18319d70fb06858c5717806e78f
Giuliano <yularossi@gmail.com>
2026-05-26 12:09:15 -0300

    Blog List Frontend, step 7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,13 +1,40 @@
+import { useState } from 'react'
+
 const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [detailsVisible, setDetailsVisible] = useState(false)
+  const toggleDetails = () => {
+    setDetailsVisible(!detailsVisible)
+  }
+  const detailsStyle = { display: detailsVisible ? '' : 'none' }
+
   return (
-    <div className="blog-item">
-      <strong>{blog.title}</strong>
-      <div>{blog.author}</div>
-      <div>likes: {blog.likes}</div>
+    <div style={blogStyle} className="blog">
       <div>
-        <a href={blog.url} target="_blank" rel="noreferrer">
-          {blog.url}
-        </a>
+        <strong>{blog.title}</strong>
+        <button onClick={toggleDetails}>
+          {detailsVisible ? 'hide' : 'view'}
+        </button>
+      </div>
+      <div style={detailsStyle}>
+        <div>
+          <a href={blog.url} target="_blank" rel="noreferrer">
+            {blog.url}
+          </a>
+        </div>
+        <div>
+          likes: {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.author}</div>
       </div>
     </div>
   )
